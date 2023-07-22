bfd0af5f7f7531c663c8d513a3a83e3e308ec3e6
angDecoder <angshudas012@gmail.com>
2023-07-22 07:07:27 +0530

    feat : layout component created

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React,{ useState,useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import './Layout.css';
+// import './Layout.css';
 import Chats from '../Chats/Chats';
 import ContactList from '../Contacts/ContactList';
 
@@ -9,7 +9,7 @@ function Layout() {
   const [currentChat, setCurrentChat] = useState('nothing');
 
   return (
-    <div id='Layout'>
+    <div id='Layout' style={{ display : 'flex' }}>
       <ContactList  />
       <Outlet />
       {/* <Chats currentChat={currentChat} /> */}
